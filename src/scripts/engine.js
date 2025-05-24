d7b0c36632a9b77725daa648d707afcca02a2bd0
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-05-24 10:18:25 -0300

    Added functions to check if the cards match

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -20,5 +20,39 @@ for (let i = 0; i < shuffleEmojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
 }
+
+// Function to handle card click
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if (openCards.length === 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+// Function to check if the two open cards match
+// If they match, add a class to keep them open
+function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+    openCards = [];
+
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        setTimeout(() => {
+            alert("You win!");
+            location.reload();
+        }, 500);
+    }
+}
+
